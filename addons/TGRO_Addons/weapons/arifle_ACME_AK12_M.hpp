2c001fa1a622643aa1dfc799e168a19526784b46
ajeless <ajeless@gmail.com>
2020-12-25 23:52:57 -0500

    Update ACME AK12 performance

--- addons/TGRO_Addons/weapons/arifle_ACME_AK12_M.hpp
+++ addons/TGRO_Addons/weapons/arifle_ACME_AK12_M.hpp
@@ -11,4 +11,13 @@ class arifle_ACME_AK12_M: arifle_ACME_base_F
         "\A3\Weapons_F_Exp\Rifles\AK12\Data\AK12_ak12_1_co.paa",
         "\A3\Weapons_F_Exp\Rifles\AK12\Data\AK12_ak12_2_co.paa"
     };
+    magazineReloadSwitchPhase=0.47999999;
+    htMin=7;
+    htMax=880;
+    inertia=0.3;
+    aimTransitionSpeed=1.6;
+    dexterity=2.8;
+    initSpeed=1500;
+    recoil="recoil_ACME_AK12_M";
+    maxZeroing=2000;
 };
