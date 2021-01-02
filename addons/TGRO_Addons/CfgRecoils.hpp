b0ab83f75776e318ffb4ec1847e10dcc756a4585
ajeless <ajeless@gmail.com>
2021-01-02 15:49:31 -0500

    Update ROF and Recoil

--- addons/TGRO_Addons/CfgRecoils.hpp
+++ addons/TGRO_Addons/CfgRecoils.hpp
@@ -3,9 +3,8 @@ class CfgRecoils
 	class recoil_default;
 	class recoil_ACME_MCR21: recoil_default
 	{
- 		muzzleOuter[]={0.40000001,0.90000001,0.50000001,0.40000001};
+ 		muzzleOuter[]={0.40000001,0.90000001,0.50000001,0.30000001};
 		kickBack[]={0.039999999,0.069999999};
-		temporary=0.0059999999;
-		permanent=.2;
+		temporary=0.0049999999;
    	};
 };
