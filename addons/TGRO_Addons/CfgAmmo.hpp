cfa0f0a3a788383e7edfb53875fceb9e5d84e9f2
ajeless <ajeless@gmail.com>
2021-02-08 23:22:12 -0500

    Update

--- addons/TGRO_Addons/CfgAmmo.hpp
+++ addons/TGRO_Addons/CfgAmmo.hpp
@@ -62,4 +62,71 @@ class CfgAmmo
 		tracerEndTime=1;
 		airFriction=-0.00060999999;
 	};
+
+	class B_65x39_ACME_Caseless_Base: BulletBase
+	{
+		hit=13.76;
+		indirectHit=0;
+		indirectHitRange=0;
+		cartridge="";
+		aiAmmoUsageFlags=64;
+		dangerRadiusBulletClose=8;
+		dangerRadiusHit=12;
+		suppressionRadiusBulletClose=6;
+		suppressionRadiusHit=8;
+		cost=1.2;
+		thrust=210;
+		thrustTime=1.5;
+		timeToLive=6;
+		airLock=1;
+		typicalSpeed=820;
+		caliber=1.5;
+		// model="\A3\Weapons_f\Data\bullettracer\tracer_green";
+		tracerScale=1;
+		tracerStartTime=0.00750000001;
+		tracerEndTime=5;
+		airFriction=-0.00089999998;
+		class CamShakeExplode
+		{
+			power=2.4494901;
+			duration=0.40000001;
+			frequency=20;
+			distance=7.3484702;
+		};
+		class CamShakeHit
+		{
+			power=6;
+			duration=0.40000001;
+			frequency=20;
+			distance=1;
+		};
+	};
+	class B_65x39_ACME_Caseless_red: B_65x39_ACME_Caseless_Base
+	{
+		model="\A3\Weapons_f\Data\bullettracer\tracer_red";
+	};
+	class B_65x39_ACME_Caseless_green: B_65x39_ACME_Caseless_Base
+	{
+		model="\A3\Weapons_f\Data\bullettracer\tracer_green";
+	};
+	class B_65x39_ACME_Caseless_yellow: B_65x39_ACME_Caseless_Base
+	{
+		model="\A3\Weapons_f\Data\bullettracer\tracer_yellow";
+	};
+	class B_65x39_ACME_Case: B_65x39_ACME_Caseless_Base
+	{
+		cartridge="FxCartridge_65";
+	};
+	class B_65x39_ACME_Case_red: B_65x39_ACME_Caseless_Base
+	{
+		model="\A3\Weapons_f\Data\bullettracer\tracer_red";
+	};
+	class B_65x39_ACME_Case_green: B_65x39_ACME_Caseless_Base
+	{
+		model="\A3\Weapons_f\Data\bullettracer\tracer_green";
+	};
+	class B_65x39_ACME_Case_yellow: B_65x39_ACME_Caseless_Base
+	{
+		model="\A3\Weapons_f\Data\bullettracer\tracer_yellow";
+	};
 };
--- addons/TGRO_Addons/CfgWeapons.hpp
+++ addons/TGRO_Addons/CfgWeapons.hpp
@@ -5,7 +5,25 @@ class MuzzleSlot;
 class SlotInfo;
 class CowsSlot;
 class PointerSlot;
-class UnderBarrelSlot;
+// class UnderBarrelSlot;
+class UnderBarrelSlot: SlotInfo
+{
+	linkProxy="\A3\Data_F_Mark\Proxies\Weapon_Slots\UNDERBARREL";
+	compatibleItems[]=
+	{
+		"bipod_01_F_snd",
+		"bipod_01_F_blk",
+		"bipod_01_F_mtp",
+		"bipod_01_F_khk",
+		"bipod_02_F_blk",
+		"bipod_02_F_tan",
+		"bipod_02_F_hex",
+		"bipod_03_F_blk",
+		"bipod_03_F_oli"
+	};
+	iconPicture="\A3\Weapons_F_Mark\Data\UI\attachment_under.paa";
+	iconPinpoint="Bottom";
+};
 class CfgWeapons
 {
 	
@@ -26,7 +44,7 @@ class CfgWeapons
 	// #include "weapons\arifle_ACME_MCR21.hpp"
 	#include "weapons\mmg_ACME_ZK21.hpp"
 	#include "weapons\arifle_ACME_HAR22.hpp"
-
+	#include "weapons\arifle_ACME_MX_V.hpp"
 	
 	class HeadgearItem;
 	class ItemCore;
